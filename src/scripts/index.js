9b4fa399acea5320426ff14fbb97238f13767a96
JadenKoor <dazekamikadze19962002@gmail.com>
2021-06-25 17:28:09 +0300

    main sait done

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,6 +3,14 @@ document.querySelector('.header-nav__theme').addEventListener('click', () => {
   document.querySelector('html').classList.toggle('dark-theme')
 })
 
+document.querySelector('.header-menu__item .header-nav__theme').addEventListener('click', () => {
+  document.querySelector('html').classList.toggle('dark-theme')
+})
+
 document.querySelector('.header-nav__burger').addEventListener('click', () => {
+  document.querySelector('.header__nav').classList.toggle('header__nav--open')
+})
+
+document.querySelector('.mobile-menu__item:last-child .mobile-menu__link').addEventListener('click', () => {
   document.querySelector('.header__nav').classList.toggle('header__nav--open')
 })
